9542ee749e274f1675dd3a896c513484d8a780a6
Jhordy272 <jaagatonp@udistrital.edu.co>
2024-06-18 16:35:58 -0400

    Creación entidad rol

---
+++ src/main/java/com/reservation/security/entity/RolEntity.java
@@ -0,0 +1,26 @@
+package com.reservation.security.entity;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "rol")
+public class RolEntity {
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+}
