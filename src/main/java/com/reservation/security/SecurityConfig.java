9f421e63704988e7c53691db78f9de1f5fde4120
Jhordy272 <jaagatonp@udistrital.edu.co>
2024-06-19 19:28:03 -0400

    Modificar SecurityConfig

--- src/main/java/com/reservation/security/SecurityConfig.java
+++ src/main/java/com/reservation/security/SecurityConfig.java
@@ -1,31 +1,45 @@
 package com.reservation.security;
 
-import lombok.RequiredArgsConstructor;
-import static org.springframework.aot.generate.ValueCodeGenerator.withDefaults;
+import com.reservation.security.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authProvider;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf
                         -> csrf
-                        .disable()
-                )
+                        .disable())
                 .authorizeHttpRequests(authRequest
                         -> authRequest
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .sessionManagement(sessionManager
+                        -> sessionManager
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
-    }
 
+    }
+    
 }
