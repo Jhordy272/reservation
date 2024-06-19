4994365efa63ea9be7b3ee571712d1b7c092e8a3
Jhordy272 <jaagatonp@udistrital.edu.co>
2024-06-19 19:39:20 -0400

    Primer version del controlador

--- src/main/java/com/reservation/controller/mainController.java
+++ src/main/java/com/reservation/controller/mainController.java
@@ -1,7 +1,12 @@
 package com.reservation.controller;
 
+import com.reservation.security.entity.UserEntity;
+import com.reservation.security.repository.UserRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class mainController {
     
+    @Autowired
+    UserRepository userRepository;
+    
     @GetMapping("")
-    public ResponseEntity<?> getClima(){
-        return new ResponseEntity<>("Te amo mucho!!",HttpStatus.OK);
+    public ResponseEntity<?> getUsers(){
+        List<UserEntity> listUsers = userRepository.findAll();
+        return new ResponseEntity<>(listUsers,HttpStatus.OK);
     }
 }
