49cc66eb413562baedb1548a39888d5bccad9d53
Jhordy272 <jaagatonp@udistrital.edu.co>
2024-06-19 19:33:12 -0400

    Configurar repositorio de usuarios

--- src/main/java/com/reservation/security/repository/UserRepository.java
+++ src/main/java/com/reservation/security/repository/UserRepository.java
@@ -4,8 +4,15 @@ import com.reservation.security.entity.UserEntity;
 import java.util.Optional;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-    Optional<UserEntity> findByUsername(String username);
+
+    @Query("SELECT e FROM UserEntity e WHERE e.username = :username")
+    Optional<UserEntity> findByUsername(@Param("username") String username);
+
+    @Query("SELECT MAX(e.id) FROM UserEntity e")
+    int findMaxId();
 }
